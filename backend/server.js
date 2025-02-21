80b64bc596a59a6c365b8a926f4224c3bfade629
Ben Corrado <bcorrado33@gmail.com>
2025-02-21 14:54:33 -0700

    ssl config in DB

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false }
 });
 
 // 1) Test endpoint
